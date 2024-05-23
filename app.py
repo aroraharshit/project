b3cca4beb03c70a5611d385efb62ff800625614f
Harshit Arora <86155436+aroraharshit@users.noreply.github.com>
2024-05-23 19:14:30 +0530

    Update Flask app to use environment variable for port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
 import numpy as np
 import pandas as pd
 
@@ -37,4 +38,5 @@ def predict():
     return jsonify({'prediction': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
